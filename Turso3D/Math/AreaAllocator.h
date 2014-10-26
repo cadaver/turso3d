9e4f3cbac865d696cdfdf49496f809ef12dd896b
Lasse Öörni <loorni@gmail.com>
2014-10-26 18:03:40 +0200

    Improved AreaAllocator (accurate and fast mode) from Urho3D.

--- Turso3D/Math/AreaAllocator.h
+++ Turso3D/Math/AreaAllocator.h
@@ -15,14 +15,15 @@ public:
     /// Default construct with empty size.
     AreaAllocator();
     /// Construct with given width and height.
-    AreaAllocator(int width, int height);
-    /// Construct with given width and height, and set the maximum allowed to grow to.
-    AreaAllocator(int width, int height, int maxWidth, int maxHeight);
-    
+    AreaAllocator(int width, int height, bool fastMode = true);
+    /// Construct with given width and height, and set the maximum it is allowed to grow to.
+    AreaAllocator(int width, int height, int maxWidth, int maxHeight, bool fastMode = true);
+
     /// Reset to given width and height and remove all previous allocations.
-    void Reset(int width, int height);
+    void Reset(int width, int height, int maxWidth = 0, int maxHeight = 0, bool fastMode = true);
     /// Try to allocate a rectangle. Return true on success, with x & y coordinates filled.
     bool Allocate(int width, int height, int& x, int& y);
+
     /// Return the current size.
     const IntVector2& Size() const { return size; }
     /// Return the current width.
@@ -35,8 +36,15 @@ public:
     int MaxWidth() const { return maxSize.x; }
     /// Return the maximum height.
     int MaxHeight() const { return maxSize.y; }
+    /// Return whether uses fast mode. Fast mode uses a simpler allocation scheme which may waste free space, but is OK for eg. fonts.
+    bool IsFastMode() const { return fastMode; }
 
 private:
+    /// Remove space from a free rectangle. Return true if the original rectangle should be erased from the free list. Not called in fast mode.
+    bool SplitRect(IntRect original, const IntRect& reserve);
+    /// Clean up redundant free space. Not called in fast mode.
+    void Cleanup();
+
     /// Free rectangles.
     Vector<IntRect> freeAreas;
     /// Current size.
@@ -45,6 +53,8 @@ private:
     IntVector2 maxSize;
     /// The dimension used for next growth. Used internally.
     bool doubleWidth;
+    /// Fast mode flag.
+    bool fastMode;
 };
 
 }
